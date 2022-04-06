5c158b8c68603bf63240fcfef78401a3786c7e13
MFC <mfatihceylan87@gmail.com>
2022-04-06 10:38:55 +0300

    ggg

---
+++ src/main/java/tr/gov/sayistay/entity/Emlakci.java
@@ -0,0 +1,29 @@
+package tr.gov.sayistay.entity;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Entity
+@Data
+@ToString()
+public class Emlakci {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(length = 30, nullable = false)
+	private String adi;
+	@Column(length = 30, nullable = false)
+	private String soyadi;
+	@OneToMany(mappedBy = "emlakci")
+	private Set<Ev> evler;
+
+}
